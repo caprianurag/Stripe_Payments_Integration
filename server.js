e95a8df51ea3d071762b47b5df644224ca9f9424
Anurag Juneja <caprianurag@yahoo.com>
2020-06-23 16:51:09 -0700

    Add files via upload

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const app = express();
+const { resolve } = require("path");
+// This is your real test secret API key.
+const stripe = require("stripe")("sk_test_5IzJ6PVCqYiV5ECz2SwYrSog00vcZmpilp");
+
+app.use(express.static("."));
+app.use(express.json());
+
+const calculateOrderAmount = items => {
+  // Replace this constant with a calculation of the order's amount
+  // Calculate the order total on the server to prevent
+  // people from directly manipulating the amount on the client
+  return 1400;
+};
+
+app.post("/create-payment-intent", async (req, res) => {
+  const { items } = req.body;
+  // Create a PaymentIntent with the order amount and currency
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: calculateOrderAmount(items),
+    currency: "usd",
+    metadata: {integration_check: 'accept_a_payment'}
+  });
+
+  res.send({
+    clientSecret: paymentIntent.client_secret
+  });
+});
+
+app.listen(4242, () => console.log('Node server listening on port 4242!'));
---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
+import CheckoutForm from "./CheckoutForm";
+
+
+// Make sure to call loadStripe outside of a component’s render to avoid
+// recreating the Stripe object on every render.
+// loadStripe is initialized with your real test publishable API key.
+const promise = loadStripe("pk_test_xRa8cDrr9aFqvzX9VGNgQYPt00mkMn0Vfv");
+
+export default function App() {
+  return (
+    <div className="App">
+      <Elements stripe={promise}>
+        <CheckoutForm />
+      </Elements>
+    </div>
+  );
+}
